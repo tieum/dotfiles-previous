cb3bbbb6f4dbe67caf05d0a698acbb7df2f2a97f
Matthieu Simon <matthieu.simon@gmail.com>
2017-06-24 21:26:21 +0200

    [wip] add install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -s $HOME/dotfiles/.config/i3/config $HOME/.config/i3/config
+ln -s $HOME/dotfiles/scripts/i3blocks $HOME/scripts/i3blocks
+ln -s $HOME/dotfiles/.config/wallpapers $HOME/.config/wallpapers
