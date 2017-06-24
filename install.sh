afa193948829474b22dce0cb5fdd5a1f451c188e
Matthieu Simon <matthieu.simon@gmail.com>
2017-06-25 00:31:30 +0200

    update install script (submodule update)

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 ln -s $HOME/dotfiles/.config/i3/config $HOME/.config/i3/config
 ln -s $HOME/dotfiles/scripts/i3blocks $HOME/scripts/i3blocks
 ln -s $HOME/dotfiles/.config/wallpapers $HOME/.config/wallpapers
 ln -s $HOME/dotfiles/.vim $HOME/.vim
+cd $HOME/.vim
+git submodule init
+git submodule update
