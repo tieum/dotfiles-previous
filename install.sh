3ab30df31c3c2471f9c35c280c44fa17070b4974
Matthieu Simon <matthieu.simon@gmail.com>
2017-10-23 17:32:27 +0200

    refactor

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-mkdir $HOME/scripts
-mkdir $HOME/.config
-ln -s $PWD/.config/i3/config $HOME/.config/i3/config
-ln -s $PWD/scripts/i3blocks $HOME/scripts/i3blocks
-ln -s $PWD/.i3blocks.conf $HOME/.i3blocks.conf
-ln -s $PWD/config/wallpapers $HOME/.config/wallpapers
-ln -s $PWD/.vim $HOME/.vim
+mkdir -p $HOME/.config/i3/config
+mkdir -p $HOME/scripts/i3blocks
+mkdir -p $HOME/.config/wallpapers
+
+ln -s $HOME/dotfiles/i3blocks.conf $HOME/i3blocks.conf
+ln -s $HOME/dotfiles/.config/i3/config $HOME/.config/i3/config
+ln -s $HOME/dotfiles/scripts/i3blocks $HOME/scripts/i3blocks
+ln -s $HOME/dotfiles/.config/wallpapers $HOME/.config/wallpapers
+ln -s $HOME/dotfiles/.vim $HOME/.vim
 cd $HOME/.vim
 git submodule init
 git submodule update
