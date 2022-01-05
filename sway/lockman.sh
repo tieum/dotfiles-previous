af8e1c0e8b415b4e72e7a46d669101e145bee12c
Matthieu Simon <matthieu.simon@gmail.com>
2022-01-05 09:52:01 +0100

    misc improvements

--- sway/lockman.sh
+++ sway/lockman.sh
@@ -4,6 +4,6 @@ swayidle \
     timeout 10 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
-swaylock --clock --indicator --screenshots --effect-scale 0.4 --effect-vignette 0.2:0.5 --effect-blur 4x2 --datestr "%a %e.%m.%Y" --timestr "%k:%M"
+swaylock --clock --indicator --screenshots --effect-scale 0.4 --effect-vignette 1:1 --effect-blur 3x3 --datestr "%a %e.%m.%Y" --timestr "%k:%M"
 # Kills last background task so idle timer doesn't keep running
 kill %%
